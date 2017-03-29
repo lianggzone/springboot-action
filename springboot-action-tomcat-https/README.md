e626e62ba17a6e353715c414192c869979101b40
lianggzone <lianggzone@163.com>
2017-03-29 08:29:12 +0800

    tomcat https

---
+++ springboot-action-tomcat-https/README.md
@@ -0,0 +1,3 @@
+# springboot-action-tomcat-https
+
+Spring Boot 揭秘与实战 源代码（HTTPS配置 实战案例）
---
+++ springboot-action-tomcat-https/src/main/java/com/lianggzone/springboot/web/tomcat/WebMain.java
@@ -0,0 +1,30 @@
+package com.lianggzone.springboot.web.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <h3>概要:</h3><p>WebMain</p>
+ * <h3>功能:</h3><p>WebMain</p>
+ * <h3>履历:</h3>
+ * <li>2017年03月28日  v0.1 版本内容: 新建</li>
+ * @author 粱桂钊
+ * @since 0.1
+ */
+@RestController
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.lianggzone.springboot" })
+public class WebMain {
+	
+    @RequestMapping("/demo/hello")
+    String home() {
+        return "Hello World!";
+    }
+    
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WebMain.class, args);
+	}
+}
